ad5236eaccb50ce8e0a618c30a7c4201d2fb28b3
juan <juanpabloosorio2406@gmail.com>
2024-10-21 15:19:51 -0500

    listo

--- dart/10.dart
+++ dart/10.dart
@@ -1,14 +1,36 @@
-class Car {
-  String? marca = "TOYOTA";
-  int placa = 123456;
-  String? modelo = "COROLLA";
-
-  Car() {
-    print("Carro criado");
-  }
+class Vehiculo {
+  String? marca;
+  int? peso_kg;
+  int? velocidad_km_h;
 }
 
-void main() {
-  Car car = Car();
-  print(car);
+class Trabajo {
+  String? sobre;
+  int? horas;
+  String? tipo;
+}
+
+class Celular {
+  String? modelo;
+  int? precio;
+  String? capacidades;
+}
+
+void main () {
+
+  Vehiculo carro = new Vehiculo();
+  carro.marca = "Toyota";
+  carro.peso_kg = 1200;
+  carro.velocidad_km_h = 300;
+
+  Trabajo informatica = new Trabajo();
+  informatica.sobre = "hacer mantenimiento a los equipos de la empresa";
+  informatica.horas = 8;
+  informatica.tipo = "Presencial";
+
+  Celular xiaomi = new Celular();
+  xiaomi.modelo = "redmi note 13c";
+  xiaomi.precio = 600000;
+  xiaomi.capacidades = "128gb de rom, 4gb de ram, procesador mediaTek helio G85, 8 nucleos";
+
 }
