39c169775b65ac465f657cf3fd56c6dfd7d0830a
juan <juanpabloosorio2406@gmail.com>
2024-11-27 17:32:10 -0500

    ok

--- js/script.js
+++ js/script.js
@@ -1,17 +1,24 @@
-function buscar (){
-    fetch('https://jsonplaceholder.typicode.com/todos/1')
-  .then(response => response.json())
-  .then(json => {
-   var a = document.querySelector("body > main > table > tbody > tr:nth-child(1) > td:nth-child(2)")
-   a.innerHTML = json.userId
+var id = document.getElementById("id")
+var button = document.getElementById("button")
 
-    var b = document.querySelector("body > main > table > tbody > tr:nth-child(2) > td:nth-child(2)")
-    b.innerHTML = json.id;
+button.addEventListener("click", function(){
+    var inputId = id.value;
 
-    var c = document.querySelector("body > main > table > tbody > tr:nth-child(3) > td:nth-child(2)")
-      c.innerHTML = json.title;
-    var d =   document.querySelector("body > main > table > tbody > tr:nth-child(4) > td:nth-child(2)")
-       d.innerHTML = json.completed
-  })
+        fetch('https://jsonplaceholder.typicode.com/todos/'+ inputId)
+      .then(response => response.json())
+      .then(json => {
+         var a = document.querySelector("body > main > table > tbody > tr:nth-child(1) > td:nth-child(2)")
+         a.innerHTML = json.userId
+         
+          var b = document.querySelector("body > main > table > tbody > tr:nth-child(2) > td:nth-child(2)")
+          b.innerHTML = json.id;
+         
+          var c = document.querySelector("body > main > table > tbody > tr:nth-child(3) > td:nth-child(2)")
+           c.innerHTML = json.title;
+          var d =   document.querySelector("body > main > table > tbody > tr:nth-child(4) > td:nth-child(2)")
+            d.innerHTML = json.completed
+        }
+    
+    )
+})
 
-  }
