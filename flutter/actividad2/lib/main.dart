6771bd342dbda7920fe9304c77926f8aa9d6d411
juan pablo <juanpabloosorio2406@gmail.com>
2025-09-09 23:09:01 -0500

    Update main.dart

--- flutter/actividad2/lib/main.dart
+++ flutter/actividad2/lib/main.dart
@@ -4,11 +4,14 @@ import 'package:flutter/material.dart';
 
 
 
+
 void main() {
-  runApp( MyApp());
+  runApp(  MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
